c6bd6d9cb786ddd682ec036b812a4c087f14bcfb
CENGIZ GUERTUSGIL <cengiz.guertusgil@exxeta.com>
2022-12-24 12:38:39 +0100

    test husky again

--- apps/backend/src/app/modules/auth/custom.strategy.ts
+++ apps/backend/src/app/modules/auth/custom.strategy.ts
@@ -1,7 +1,7 @@
-import {PassportStrategy} from '@nestjs/passport';
-import {Injectable} from '@nestjs/common';
-import {Strategy} from 'passport-custom';
-import {AuthService} from './service/auth.service';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { Strategy } from 'passport-custom';
+import { AuthService } from './service/auth.service';
 
 @Injectable()
 export class CustomStrategy extends PassportStrategy(Strategy) {
